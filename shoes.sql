c6b1e639acdc38ccffcbfa37879553f71e9bb50a
Nanda Surya <nandasurya@Mac-mini.local>
2024-12-26 02:47:03 +0800

    fix

--- shoes.sql
+++ shoes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 23, 2024 at 11:38 AM
+-- Generation Time: Dec 25, 2024 at 07:45 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -20,6 +20,95 @@ SET time_zone = "+00:00";
 --
 -- Database: `shoes`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id_admin` int(11) NOT NULL,
+  `nama_admin` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id_admin`, `nama_admin`, `password`, `created_at`) VALUES
+(1, 'admin', '$2y$10$/iXYBNLGvn6G/EDghDHpJeGDhy14sc3jjTCrUj9pBT7FaQcX4.7Im', '2024-12-25 18:28:42');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `id_pembayaran` int(11) NOT NULL,
+  `id_transaksi` int(11) NOT NULL,
+  `id_pelanggan` int(11) NOT NULL,
+  `total_pembayaran` int(11) NOT NULL,
+  `tanggal_pembayaran` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pembayaran`
+--
+
+INSERT INTO `pembayaran` (`id_pembayaran`, `id_transaksi`, `id_pelanggan`, `total_pembayaran`, `tanggal_pembayaran`) VALUES
+(1, 2, 2, 80000, '2024-12-23 12:00:02'),
+(2, 3, 2, 180000, '2024-12-23 12:22:34'),
+(3, 4, 3, 80000, '2024-12-25 17:40:18');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `service`
+--
+
+CREATE TABLE `service` (
+  `id_service` int(11) NOT NULL,
+  `nama_service` varchar(100) NOT NULL,
+  `harga` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `service`
+--
+
+INSERT INTO `service` (`id_service`, `nama_service`, `harga`) VALUES
+(1, 'Easy Clean', 20000.00),
+(2, 'Medium Clean', 40000.00),
+(3, 'Hard Clean', 60000.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `id_pelanggan` int(11) NOT NULL,
+  `id_service` int(11) NOT NULL,
+  `jumlah_sepatu` int(11) NOT NULL,
+  `merk_sepatu` varchar(50) NOT NULL,
+  `warna_sepatu` varchar(30) NOT NULL,
+  `tanggal_penjemputan` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `id_pelanggan`, `id_service`, `jumlah_sepatu`, `merk_sepatu`, `warna_sepatu`, `tanggal_penjemputan`) VALUES
+(2, 2, 2, 2, 'adidas', 'putih', '2024-12-21'),
+(3, 2, 3, 3, 'vans', 'black', '2024-12-27'),
+(4, 3, 2, 2, 'adidas', 'biru', '2024-12-28');
 
 -- --------------------------------------------------------
 
@@ -42,12 +131,41 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `address`, `phone`, `created_at`) VALUES
-(1, 'aguser', 'agus123@gmail.com', '$2y$10$Yf8uqHavfuosiyyzZZUAge982f.pE8z.ApAY7D6QN8wb1Xw3YOrz6', 'rfedfdf', '08123456786', '2024-12-23 06:38:34'),
-(2, 'nandas', 'nanda12@gmail.com', '$2y$10$Ylljq8ax7kCMZ35o3YLR8Os8Kfveati3HM0MRy5OP57QWPNZiVIDC', 'dfsaadfsf', '0812345678', '2024-12-23 07:29:24');
+(2, 'nandas', 'nanda1234@gmail.com', '$2y$10$Ylljq8ax7kCMZ35o3YLR8Os8Kfveati3HM0MRy5OP57QWPNZiVIDC', 'aidadajdka', '082347757499', '2024-12-23 07:29:24'),
+(3, 'nandas', 'nanda12@gmail.com', '$2y$10$Dvlv7oM87ww7TtlJxXeD0.7ySWAzg83chlu/xWZDFWJ2IO.El54Jy', 'sdsd', '08123456786', '2024-12-25 17:22:59'),
+(4, 'ads', 'ads1@gmail.com', '$2y$10$AhIz.3HKkTu3gx79SUpooeNoyVEY/rhhbfVzQ0D6ZKk9P5sx2rA52', 'asdasd', '0812345678', '2024-12-25 18:36:43');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`id_pembayaran`),
+  ADD KEY `id_transaksi` (`id_transaksi`),
+  ADD KEY `id_pelanggan` (`id_pelanggan`);
+
+--
+-- Indexes for table `service`
+--
+ALTER TABLE `service`
+  ADD PRIMARY KEY (`id_service`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `id_pelanggan` (`id_pelanggan`),
+  ADD KEY `id_service` (`id_service`);
 
 --
 -- Indexes for table `users`
@@ -61,10 +179,52 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  MODIFY `id_pembayaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `service`
+--
+ALTER TABLE `service`
+  MODIFY `id_service` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD CONSTRAINT `pembayaran_ibfk_1` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`),
+  ADD CONSTRAINT `pembayaran_ibfk_2` FOREIGN KEY (`id_pelanggan`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_pelanggan`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`id_service`) REFERENCES `service` (`id_service`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
